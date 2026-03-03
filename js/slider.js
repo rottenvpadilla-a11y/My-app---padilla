fb58370acdcd3cbe1c4bb0472451da20c7a70994
rottenvpadilla-a11y <rottenvpadilla@gmail.com>
2026-03-03 15:22:39 +0800

    Mine

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+const slider = tns({
+  container: ".testimonialSlider",
+  items: 1,
+  slideBy: 1,
+  speed: 100,
+  autoplay: true,
+  nab: true,
+  navPosition: "bottom",
+  autoplayButtonOutput: false,
+  rewind: true,
+  controlsContainer: "#controls",
+  prevButton: ".prev",
+  nextButton: ".next",
+});
---
+++ js/tab.js
@@ -0,0 +1,27 @@
+const tabButtons = document.querySelectorAll(".eventTab");
+const tabContents = document.querySelectorAll(".eventItem");
+
+tabButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    clearActiveTabs();
+    clearActiveContents();
+    button.classList.add("active");
+
+    const correspondingContent = document.querySelector(`#${button.id}-detail`);
+    if (correspondingContent) {
+      correspondingContent.classList.add("active");
+    }
+  });
+});
+
+function clearActiveTabs() {
+  tabButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+}
+
+function clearActiveContents() {
+  tabContents.forEach((content) => {
+    content.classList.remove("active");
+  });
+}
